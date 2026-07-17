f54dd217b1d3ff3b2c18693728519547bd8181ce
Alexandru Dragut <alexandru.marius.dragut@outlook.com>
2026-07-17 19:00:35 +0300

    change fonts and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,45 +8,15 @@ module.exports = {
     extend: {
       colors: {
         forest: {
-          50: "#f2f7f0",
-          100: "#e5efdf",
-          200: "#d9e8ce",
-          300: "#c8e0bc",
-          400: "#b7d9ab",
-          500: "#a6d19a",
-          600: "#91c189",
-          700: "#7cc178",
-          800: "#67b167",
-          900: "#5ca156",
-          950: "#537f48",
+          950: "#14342B",
         },
         sand: {
-          50: "#fcf5f1",
-          100: "#fae9db",
-          200: "#f8daba",
-          300: "#f5c899",
-          400: "#f3b577",
-          500: "#f1a456",
-          600: "#ef9234",
-          700: "#ed8113",
-          800: "#eb6f02",
-          900: "#c97a53",
-          950: "#a46445",
+          900: "#C29F7C",
+          400: "#E5D3B3",
         },
         cream: {
-          50: "#fefdfc",
-          100: "#fdf9f5",
-          200: "#f7f0e9",
-          300: "#f2eadd",
-          400: "#edebf2",
-          500: "#e9e3e0",
-          600: "#e5ddd8",
-          700: "#dfcdc5",
-          800: "#d9ccc0",
-          900: "#d3bbb9",
-          950: "#cdb5b3",
+          50: "#FAF8F5",
         },
-        warmwhite: "#faf6f0",
       },
       fontFamily: {
         heading: ["Playfair Display", "serif"],
@@ -59,7 +29,7 @@ module.exports = {
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0' },
-          '100%': { opacity: '0' },
+          '100%': { opacity: '1' },
         },
         slideUp: {
           '0%': { transform: 'translateY(20px)', opacity: '0' },
