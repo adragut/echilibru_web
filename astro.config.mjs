b8e30b85973327ad15a65d4d82c841771bd01321
Alexandru Dragut <alexandrudragut@example.com>
2026-08-15 15:45:39 +0300

    cloudflare migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://echilibru.ro',
   output: 'server',
-  adapter: cloudflare({
-    // Add any necessary adapter options here
-  }),
+  adapter: cloudflare(),
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          'tailwindcss',
+          'autoprefixer',
+        ],
+      },
+    },
+  },
 });
