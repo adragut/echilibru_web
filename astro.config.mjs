d9b81b693a0e9e90f3889d3a493c2fb1da1d2c4c
Alexandru Dragut <alexandrudragut@example.com>
2026-08-16 20:30:25 +0300

    update CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,8 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import postcssImport from 'postcss-import';
 
 export default defineConfig({
   site: 'https://echilibru.ro',
   output: 'server',
   adapter: cloudflare(),
-  vite: {
-    css: {
-      postcss: {
-        plugins: [
-          postcssImport(),
-          tailwindcss(),
-          autoprefixer(),
-        ],
-      },
-    },
-  },
 });
